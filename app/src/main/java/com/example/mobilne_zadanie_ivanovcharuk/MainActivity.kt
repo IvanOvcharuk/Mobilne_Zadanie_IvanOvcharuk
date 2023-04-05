4fd080008a367ea08e63e6dd696373f3be5602fc
IvanOvcharuk <ivanovcharuk228@gmail.com>
2023-04-05 23:10:40 +0200

    Strona logowania gotowa

--- app/src/main/java/com/example/mobilne_zadanie_ivanovcharuk/MainActivity.kt
+++ app/src/main/java/com/example/mobilne_zadanie_ivanovcharuk/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import com.google.android.material.navigation.NavigationView
@@ -17,6 +18,10 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         val navigationView = findViewById<NavigationView>(R.id.nav_view)
         navigationView.visibility = View.VISIBLE
+
+        var zalogowano = false
+        var tablica = arrayOf("","","","")
+
 
         findViewById<Button>(R.id.btnwyjdz).setOnClickListener {
             navigationView.visibility = View.GONE
@@ -35,12 +40,49 @@ class MainActivity : AppCompatActivity() {
 
 
         findViewById<Button>(R.id.oceny).setOnClickListener {
-            startActivity(Intent(this, Oceny::class.java))
+            if(zalogowano==true)
+                startActivity(Intent(this, Oceny::class.java))
+            else
+                Toast.makeText(this, "Musisz się zalogować", Toast.LENGTH_SHORT).show()
         }
 
 
         findViewById<Button>(R.id.inf).setOnClickListener {
-            startActivity(Intent(this,Informacja_Uzytkownik::class.java))
+            if(zalogowano==true)
+                startActivity(Intent(this,Informacja_Uzytkownik::class.java))
+            else
+                Toast.makeText(this, "Musisz się zalogować", Toast.LENGTH_SHORT).show()
+
         }
+        findViewById<Button>(R.id.wyslij).setOnClickListener{
+            val imie = findViewById<EditText>(R.id.imie).text.toString()
+            val nazwisko = findViewById<EditText>(R.id.nazwisko).text.toString()
+            val klasa = findViewById<EditText>(R.id.klasa).text.toString()
+            val nick = findViewById<EditText>(R.id.nick).text.toString()
+
+            if (imie.isEmpty() || nazwisko.isEmpty() || klasa.isEmpty() || nick.isEmpty())
+                Toast.makeText(this, "Wypełnij wszystkie pola", Toast.LENGTH_SHORT).show()
+            else if (imie!="Ivan" || nazwisko!="Ovcharuk")
+                Toast.makeText(this, "Niepoprawne dane", Toast.LENGTH_SHORT).show()
+            else {
+                zalogowano = true
+                tablica[0] = imie
+                tablica[1] = nazwisko
+                tablica[2] = klasa
+                tablica[3] = nick
+
+                findViewById<EditText>(R.id.imie).visibility= View.GONE
+                findViewById<EditText>(R.id.nazwisko).visibility= View.GONE
+                findViewById<EditText>(R.id.klasa).visibility= View.GONE
+                findViewById<EditText>(R.id.nick).visibility= View.GONE
+
+                Toast.makeText(this, "Zalogowano!", Toast.LENGTH_SHORT).show()
+
+            }
+
+
+        }
+
+
     }
 }
